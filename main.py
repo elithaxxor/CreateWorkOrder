02e10e54cc792ce999a384c9f7274be5a7a15fdd
adel <86808805+elithaxxor@users.noreply.github.com>
2024-07-11 02:20:02 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os, subprocess
 import datetime as dt
 import tkinter as tk
+import traceback
 from tkinter import filedialog
 from tkinter import messagebox
 import docx
@@ -38,6 +39,8 @@ class InvoiceAutomation:
         self.service_amount_label = tk.Label(self.root, text='Service Amount:')
         self.service_amount_entry = tk.Entry(self.root)
 
+        self.location_name_label = tk.Label(self.root, text='Location Name:')
+        self.location_name_entry = tk.Entry(self.root)
 
         self.service_single_amount_label = tk.Label(self.root, text='Service Single Amount:')
         self.service_single_amount_entry = tk.Entry(self.root)
@@ -47,8 +50,16 @@ class InvoiceAutomation:
 
         self.payment_method_label = tk.Label(self.root, text='Payment Method:')
 
+        self.schduled_date_label = tk.Label(self.root, text='Scheduled Date:')
+        self.scheduled_date = tk.Entry(self.root)
 
-        # Create the dropdown for the payment method
+        self.provider_name_label = tk.Label(self.root, text='Provider Name:')
+        self.provider_name = tk.Entry(self.root)
+
+        self.nte = tk.Label(self.root, text='NTE:')
+        self.nte_entry = tk.Entry(self.root)
+
+    # Create the dropdown for the payment method
         self.payment_method_entry = tk.Entry(self.root)
         self.payment_methods = {'Credit Card', 'Check', 'Wire Transfer'}
         self.payment_methods = tk.StringVar(self.root)
@@ -89,9 +100,77 @@ class InvoiceAutomation:
 
         self.payment_method_label.pack(padding_options)
 
+        self.payment_method_entry.pack(padding_options)
+        self.payment_method_dropdown.pack(padding_options)
+
+        self.nte.pack(padding_options)
+        self.nte_entry.pack(padding_options)
+
+        self.location_name_label.pack(padding_options)
+        self.location_name_entry.pack(padding_options)
+
+        self.schduled_date_label.pack(padding_options)
+        self.scheduled_date.pack(padding_options)
+
+        self.provider_name_label.pack(padding_options)
+        self.provider_name.pack(padding_options)
+
+        self.create_button.pack(padding_options)
+
+        self.root.mainloop() # Start the main loop
+
+
+
+    # Create the invoice, save it to the file system, and open it
+    @staticmethod
+    def replace_text(doc, old_text, new_text):
+        for p in doc.paragraphs:
+            if old_text in p.text:
+                p.text = p.text.replace(old_text, new_text)
+
+
+    # loads docx template file,
     def create_invoice(self):
-        pass
+        doc = docx.Document('invoice_template.docx')
+        selected_payment_method = self.payment_methods.get()
+        print("[!] payment mehtods selected: ", selected_payment_method)
+
+
+        # Replace the placeholders in the template with the user input
+
+        try:
+            self.replace_text(doc, '<<Partner>>', self.partner_entry.get())
+            self.replace_text(doc, '<<PartnerStreet>>', self.partner_street_entry.get())
+            self.replace_text(doc, '<<PartnerZipCityCountry>>', self.partner_zip_city_country_entry.get())
+            self.replace_text(doc, '<<InvoiceNumber>>', self.invoice_number_entry.get())
+            self.replace_text(doc, '<<InvoiceDate>>', self.invoice_date_entry.get())
+            self.replace_text(doc, '<<ServiceDescription>>', self.service_description_entry.get())
+            self.replace_text(doc, '<<ServiceAmount>>', self.service_amount_entry.get())
+            self.replace_text(doc, '<<ServiceSingleAmount>>', self.service_single_amount_entry.get())
+            self.replace_text(doc, '<<PaymentTerms>>', self.payment_terms_entry.get())
+            self.replace_text(doc, '<<PaymentMethod>>', selected_payment_method)
+            self.replace_text(doc, '<<ScheduledDate>>', self.scheduled_date.get())
+            self.replace_text(doc, '<<ProviderName>>', self.provider_name.get())
+            self.replace_text(doc, '<<NTE>>', self.nte_entry.get())
+            self.replace_text(doc, '<<LocationName>>', self.location_name_entry.get())
+            self.replace_text(doc, '<<PaymentMethod>>', selected_payment_method)
+            self.replace_text(doc, '<<ScheduledDate>>', self.scheduled_date.get())
+
+            # Save the invoice to the file system
+
+        except Exception as e:
+            messagebox.showerror('Error', f'Error replacing placeholders: {e} \n {traceback.format_exc()}')
+            print(f'Error replacing placeholders:  {e}')
+            print(traceback.format_exc())
+
+
 
 def create_invoice(): # Create the invoice
     pass
+
+
+if __name__ == '__main__':
+    invoice_automation = InvoiceAutomation()
+    invoice_automation.root.mainloop()
+
 
